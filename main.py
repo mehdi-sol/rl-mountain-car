dd365700dee60b167caa2a70af10102cc27d0786
Mehdi Solamannejad <mehdisolamannejad@gmail.com>
2022-08-05 10:45:03 +0100

    feat: remove seeding

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from agent import MountainCarContinuousAgent
 
 
 if __name__ == "__main__":
-    np.random.seed(0)
     agent = MountainCarContinuousAgent()
     agent.train(20)
     agent.show_off()
